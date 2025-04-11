c1e4ac01b1bf5838bea5fda6e0165c076e1e4e83
zurabkostava <50685820+zurabkostava@users.noreply.github.com>
2025-04-12 03:02:26 +0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'english-cards-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/quiz.js',
+    '/tts.js',
+    '/utils.js',
+    '/wordhear.js',
+    '/makeword.js',
+    '/mix.js',
+    '/typegame.js',
+    '/sentence.js',
+    '/puzzle.js',
+    '/icons/icon-192.png',
+    '/icons/icon-512.png'
+];
+
+// ინსტალაცია
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// ფეტჩი
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+});
