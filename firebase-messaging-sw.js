0f33be33f1a536cdcfe354f9d404e74ede6575e2
zurabkostava <50685820+zurabkostava@users.noreply.github.com>
2025-04-12 14:24:33 +0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,5 @@
 importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging.js');
 
 firebase.initializeApp({
     apiKey: "AIzaSyArftPeH-SoIwmm2aKLDHBTE8M4DQ5jLM8",
@@ -16,7 +14,7 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-    console.log('[firebase-messaging-sw.js] Background message:', payload);
+    console.log('[firebase-messaging-sw.js] მიღებულია Background Message:', payload);
 
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
