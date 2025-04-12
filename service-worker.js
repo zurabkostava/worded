84f96755fc8549dfaaa3e87b228fc148e6253611
zurabkostava <50685820+zurabkostava@users.noreply.github.com>
2025-04-12 14:53:33 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -75,3 +75,31 @@ self.addEventListener('push', function(event) {
 Notification.requestPermission().then(permission => {
     console.log("🔐 Notification permission:", permission);
 });
+
+importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyArftPeH-SoIwmm2aKLDHBTE8M4DQ5jLM8",
+    authDomain: "worded-1a455.firebaseapp.com",
+    projectId: "worded-1a455",
+    storageBucket: "worded-1a455.appspot.com",
+    messagingSenderId: "385741553786",
+    appId: "1:385741553786:web:c9e1d0d5bb662950f9fbc3",
+    measurementId: "G-9QPQ3JE2MZ"
+});
+
+const messaging = firebase.messaging();
+
+// Background push handler
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Background message:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icons/icon-192.png'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
