2cfa8e4b2f076c91c2761a0e160877b5068bc718
zurabkostava <50685820+zurabkostava@users.noreply.github.com>
2025-04-12 13:23:07 +0400

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.6.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyArftPeH-SoIwmm2aKLDHBTE8M4DQ5jLM8",
+    authDomain: "worded-1a455.firebaseapp.com",
+    projectId: "worded-1a455",
+    storageBucket: "worded-1a455.appspot.com",
+    messagingSenderId: "385741553786",
+    appId: "1:385741553786:web:c9e1d0d5bb662950f9fbc3",
+    measurementId: "G-9QPQ3JE2MZ"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('📥 [firebase-messaging-sw.js] Background msg:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        // icon: '/icons/icon-192.png' // optional
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
